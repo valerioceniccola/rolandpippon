94b2e3f0fabb96b991170d24e14d291d78bd8d08
valerioceniccola <Valeriano2>
2024-11-30 00:05:02 +0100

    update

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -9,96 +9,115 @@ import {
   Drawer,
   ScrollArea,
   Divider,
-  UnstyledButton, Box, Collapse, Title
+  UnstyledButton, Box, Collapse, Title, Text
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import classes from './Navbar.module.css';
-import { NavLink } from "react-router-dom"
-
-const links = [
-  {
-    link: '/',
-    label: 'Singolari 2023',
-    links: [
-      { link: '/tournament/1', label: 'Roland Pippon' },
-    ],
-  },
-  {
-    link: '/',
-    label: 'Singolari 2024',
-    links: [
-      { link: '/tournament/2', label: 'Roland Pippon' },
-      { link: '/tournament/3', label: '⚡️Autumn Pippon' },
-      { link: '/tournament/4', label: 'Pippon Finals' },
-    ],
-  },
-  {
-    link: '/',
-    label: 'Singolari 2025',
-    links: [
-      { link: '/tournament/5', label: 'Pippon Open' },
-      { link: '/tournament/6', label: '⚡️Spring Pippon' },
-      { link: '/tournament/7', label: 'Roland Pippon' },
-      { link: '/tournament/8', label: '⚡️Autumn Pippon' },
-      { link: '/tournament/9', label: 'Pippon Finals' },
-    ],
-  },
-  {
-    link: '/',
-    label: 'Doppi 2025',
-    links: [
-      { link: '/tournament/10', label: 'Double Pippon' },
-      { link: '/tournament/11', label: 'Pippon Davis' },
-    ],
-  },
-  { link: '/ranking', label: 'Ranking' },
-];
+import { NavLink, useNavigate } from "react-router-dom"
+import { Dropdown } from "react-nested-dropdown"
+import 'react-nested-dropdown/dist/styles.css'
 
 export function Navbar() {
 
-  const [opened, { toggle }] = useDisclosure(false);
-  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+  const navigate = useNavigate()
 
-  const items = links.map((link) => {
+  const [opened, { toggle }] = useDisclosure(false)
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false)
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false)
 
-    const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link} className={classes.dropdownItem}>
-        <NavLink
-          to={item.link}
-          className={classes.subLink}
-        >
-          {item.label}
-        </NavLink>
-      </Menu.Item>
-    ))
-
-    if (menuItems) {
-      return (
-        <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
-          <Menu.Target>
-            <NavLink
-              to={link.link}
-              className={classes.link}
-            >
-              <Title tt="uppercase" order={5}>{link.label}</Title>
-            </NavLink>
-          </Menu.Target>
-          <Menu.Dropdown className={classes.dropdown}>{menuItems}</Menu.Dropdown>
-        </Menu>
-      );
+  const links = [
+    // {
+    //   type: 'default',
+    //   label: 'Tutti i tornei',
+    //   onSelect: () => navigate('/'),
+    // },
+    {
+      type: 'dropdown',
+      label: 'Singolari',
+      data: [
+        {
+          label: '2025',
+          items: [
+            {
+              label: 'Pippon Open',
+              onSelect: () => navigate('/tournament/5'),
+            },
+            {
+              label: 'Spring Pippon ⚡',
+              onSelect: () => navigate('/tournament/6'),
+            },
+            {
+              label: 'Roland Pippon',
+              onSelect: () => navigate('/tournament/7'),
+            },
+            {
+              label: 'Autumn Pippon ⚡',
+              onSelect: () => navigate('/tournament/8'),
+            },
+            {
+              label: 'Pippon Finals',
+              onSelect: () => navigate('/tournament/9'),
+            },
+          ],
+        },
+        {
+          label: '2024',
+          items: [
+            {
+              label: 'Roland Pippon',
+              onSelect: () => navigate('/tournament/2'),
+            },
+            {
+              label: 'Autumn Pippon ⚡',
+              onSelect: () => navigate('/tournament/3'),
+            },
+            {
+              label: 'Pippon Finals',
+              onSelect: () => navigate('/tournament/4'),
+            },
+          ],
+        },
+        {
+          label: '2023',
+          items: [
+            {
+              label: 'Roland Pippon',
+              onSelect: () => navigate('/tournament/1'),
+            },
+          ],
+        },
+      ]
+    },
+    {
+      type: 'dropdown',
+      label: 'Doppi',
+      data: [
+        {
+          label: '2025',
+          items: [
+            {
+              label: 'Double Pippon',
+              onSelect: () => navigate('/tournament/10'),
+            },
+            {
+              label: 'Pippon Davis',
+              onSelect: () => navigate('/tournament/11'),
+            },
+          ],
+        }
+      ]
+    },
+    {
+      type: 'default',
+      label: 'Ranking',
+      onSelect: () => navigate('/ranking'),
+    },
+    {
+      type: 'default',
+      label: 'Tennisti',
+      onSelect: () => navigate('/players'),
     }
-
-    return (
-      <NavLink
-        key={link.label}
-        to={link.link}
-        className={classes.link}
-      >
-        <Title tt="uppercase" order={5}>{link.label}</Title>
-      </NavLink>
-    );
-  });
+  ]
 
   return (
     <Box className={classes.wrap}>
@@ -115,14 +134,48 @@ export function Navbar() {
 
       <header className={classes.header}>
 
-        {/*<Container size="md">*/}
-          <div className={classes.inner}>
-            <Group gap={5} visibleFrom="sm">
-              {items}
-            </Group>
-            <Burger opened={drawerOpened} onClick={toggleDrawer} size="sm" hiddenFrom="sm"/>
-          </div>
-        {/*</Container>*/}
+        <Box visibleFrom="sm" className={classes.headerInset}>
+          {
+            links.map((link) => {
+              if (link.type === 'dropdown') {
+                return (
+                  <Dropdown
+                    key={link.label}
+                    items={link.data!}
+                    containerWidth={140}
+                    className={classes.dropdown}
+                  >
+                    {({ isOpen, onClick }) => (
+                      <Title
+                        className={classes.link}
+                        onClick={onClick}
+                      >
+                        {link.label}
+                      </Title>
+                    )}
+                  </Dropdown>
+                )
+              } else {
+                return (
+                  <Title
+                    key={link.label}
+                    className={classes.link}
+                    onClick={link.onSelect}
+                  >
+                    {link.label}
+                  </Title>
+                )
+              }
+            })
+          }
+        </Box>
+
+        <Burger
+          opened={drawerOpened}
+          onClick={toggleDrawer}
+          size="sm"
+          hiddenFrom="sm"
+        />
 
         <Drawer
           opened={drawerOpened}
@@ -132,55 +185,78 @@ export function Navbar() {
           hiddenFrom="sm"
           zIndex={1000000}
         >
-          <ScrollArea h="calc(100vh - 80px" mx="-md">
+
+          <ScrollArea h="calc(100vh)" mx="-md">
 
             <Divider mb="sm"/>
-            <div className={classes.linksMobile}>
+
+            <Box className={classes.linksMobile}>
               {
                 links.map((link) => {
-                  if (link.links) {
+                  if (link.type === 'dropdown') {
                     return (
-                      <Box key={link.link}>
-                        <NavLink
-                          to={link.link}
+                      <Box key={link.label}>
+                        <Title
+                          tt="uppercase"
                           className={classes.link}
-                          onClick={() => closeDrawer()}
                         >
                           {link.label}
-                        </NavLink>
+                        </Title>
                         <Box className={classes.subMenu}>
-                          {link.links.map((item) => (
-                            <NavLink
-                              className={classes.link}
+                          {link.data!.map((item: any) => (
+                            <div
                               key={item.label}
-                              to={item.link}
-                              onClick={() => closeDrawer()}
+                              className={classes.subMenuBlock}
                             >
-                              {item.label}
-                            </NavLink>
+                              <Title
+                                tt="uppercase"
+                                order={6}
+                                key={item.label}
+                                onClick={item.onSelect}
+                                className={classes.subMenuTitle}
+                              >
+                                {item.label}
+                              </Title>
+                              {
+                                item.items.map((subItem: any) => (
+                                  <Title
+                                    tt="uppercase"
+                                    order={5}
+                                    key={subItem.label}
+                                    onClick={subItem.onSelect}
+                                    className={classes.subMenuItem}
+                                  >
+                                    {subItem.label}
+                                  </Title>
+                                ))
+                              }
+                            </div>
                           ))}
                         </Box>
                       </Box>
                     )
                   } else {
                     return (
-                      <NavLink
-                        key={link.link}
-                        to={link.link}
+                      <Title
+                        tt="uppercase"
+                        key={link.label}
                         className={classes.link}
-                        onClick={() => closeDrawer()}
+                        onClick={link.onSelect}
                       >
                         {link.label}
-                      </NavLink>
+                      </Title>
                     )
                   }
                 })
               }
-            </div>
+            </Box>
+
 
           </ScrollArea>
         </Drawer>
+
       </header>
+
     </Box>
   );
 }
---
+++ src/components/organisms/_Navbar/Navbar.tsx
@@ -0,0 +1,186 @@
+import {
+  Menu,
+  Group,
+  Center,
+  Burger,
+  Container,
+  Image,
+  Anchor,
+  Drawer,
+  ScrollArea,
+  Divider,
+  UnstyledButton, Box, Collapse, Title
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import classes from './Navbar.module.css';
+import { NavLink } from "react-router-dom"
+
+const links = [
+  {
+    link: '/',
+    label: 'Singolari 2023',
+    links: [
+      { link: '/tournament/1', label: 'Roland Pippon' },
+    ],
+  },
+  {
+    link: '/',
+    label: 'Singolari 2024',
+    links: [
+      { link: '/tournament/2', label: 'Roland Pippon' },
+      { link: '/tournament/3', label: '⚡️Autumn Pippon' },
+      { link: '/tournament/4', label: 'Pippon Finals' },
+    ],
+  },
+  {
+    link: '/',
+    label: 'Singolari 2025',
+    links: [
+      { link: '/tournament/5', label: 'Pippon Open' },
+      { link: '/tournament/6', label: '⚡️Spring Pippon' },
+      { link: '/tournament/7', label: 'Roland Pippon' },
+      { link: '/tournament/8', label: '⚡️Autumn Pippon' },
+      { link: '/tournament/9', label: 'Pippon Finals' },
+    ],
+  },
+  {
+    link: '/',
+    label: 'Doppi 2025',
+    links: [
+      { link: '/tournament/10', label: 'Double Pippon' },
+      { link: '/tournament/11', label: 'Pippon Davis' },
+    ],
+  },
+  { link: '/ranking', label: 'Ranking' },
+];
+
+export function Navbar() {
+
+  const [opened, { toggle }] = useDisclosure(false);
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+
+  const items = links.map((link) => {
+
+    const menuItems = link.links?.map((item) => (
+      <Menu.Item key={item.link} className={classes.dropdownItem}>
+        <NavLink
+          to={item.link}
+          className={classes.subLink}
+        >
+          {item.label}
+        </NavLink>
+      </Menu.Item>
+    ))
+
+    if (menuItems) {
+      return (
+        <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
+          <Menu.Target>
+            <NavLink
+              to={link.link}
+              className={classes.link}
+            >
+              <Title tt="uppercase" order={5}>{link.label}</Title>
+            </NavLink>
+          </Menu.Target>
+          <Menu.Dropdown className={classes.dropdown}>{menuItems}</Menu.Dropdown>
+        </Menu>
+      );
+    }
+
+    return (
+      <NavLink
+        key={link.label}
+        to={link.link}
+        className={classes.link}
+      >
+        <Title tt="uppercase" order={5}>{link.label}</Title>
+      </NavLink>
+    );
+  });
+
+  return (
+    <Box className={classes.wrap}>
+
+      <NavLink to="/">
+        <Image
+          src="/logo.svg"
+          fit="contain"
+          h={150}
+          pt={20}
+          pb={10}
+        />
+      </NavLink>
+
+      <header className={classes.header}>
+
+        {/*<Container size="md">*/}
+          <div className={classes.inner}>
+            <Group gap={5} visibleFrom="sm">
+              {items}
+            </Group>
+            <Burger opened={drawerOpened} onClick={toggleDrawer} size="sm" hiddenFrom="sm"/>
+          </div>
+        {/*</Container>*/}
+
+        <Drawer
+          opened={drawerOpened}
+          onClose={closeDrawer}
+          size="100%"
+          title="Roland Pippon"
+          hiddenFrom="sm"
+          zIndex={1000000}
+        >
+          <ScrollArea h="calc(100vh - 80px" mx="-md">
+
+            <Divider mb="sm"/>
+            <div className={classes.linksMobile}>
+              {
+                links.map((link) => {
+                  if (link.links) {
+                    return (
+                      <Box key={link.link}>
+                        <NavLink
+                          to={link.link}
+                          className={classes.link}
+                          onClick={() => closeDrawer()}
+                        >
+                          {link.label}
+                        </NavLink>
+                        <Box className={classes.subMenu}>
+                          {link.links.map((item) => (
+                            <NavLink
+                              className={classes.link}
+                              key={item.label}
+                              to={item.link}
+                              onClick={() => closeDrawer()}
+                            >
+                              {item.label}
+                            </NavLink>
+                          ))}
+                        </Box>
+                      </Box>
+                    )
+                  } else {
+                    return (
+                      <NavLink
+                        key={link.link}
+                        to={link.link}
+                        className={classes.link}
+                        onClick={() => closeDrawer()}
+                      >
+                        {link.label}
+                      </NavLink>
+                    )
+                  }
+                })
+              }
+            </div>
+
+          </ScrollArea>
+        </Drawer>
+      </header>
+    </Box>
+  );
+}
